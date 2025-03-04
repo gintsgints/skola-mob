ae32ff9ec0575299d4cf7d4fc0fd9a611cbcda02
Gints Polis <polis.gints@gmail.com>
2025-03-04 17:48:49 +0200

    init with bloat removal

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+
+const greetMsg = ref("");
+const name = ref("");
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsg.value = await invoke("greet", { name: name.value });
+}
+</script>
+
+<template>
+  <main class="container">
+    <h1>Welcome Smart outlet central</h1>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
