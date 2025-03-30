7be2d226b335401a82e73e7662c040b71aa6eb5f
Gints Polis <polis.gints@gmail.com>
2025-03-30 19:05:47 +0300

    Izņemtas blec paliekas.
    more to remove blec
    
    more blec remove

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,7 +9,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_blec::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_provision::init())
         .invoke_handler(tauri::generate_handler![greet])
